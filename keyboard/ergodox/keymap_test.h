5f660f9acfb5241fe8256a7a171c8aad4a5c9e00
Mark Imbriaco <mark@imbriaco.com>
2014-05-24 21:52:10 -0400

    simple keymap that sets every key to the letter "a". very useful for troubleshooting physical layer issues

---
+++ keyboard/ergodox/keymap_test.h
@@ -0,0 +1,24 @@
+static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    KEYMAP(  // layer 0 : default
+        // left hand
+        A, A, A, A, A, A, A,
+        A, A, A, A, A, A, A,
+        A, A, A, A, A, A,
+        A, A, A, A, A, A, A,
+        A, A, A, A, A,
+                                    A, A,
+                                       A,
+                                 A, A, A,
+        // right hand
+             A, A, A, A, A, A, A,
+             A, A, A, A, A, A, A,
+                A, A, A, A, A, A,
+             A, A, A, A, A, A, A,
+                   A, A, A, A, A,
+        A, A,
+        A,
+        A, A, A
+    ),
+};
+
+static const uint16_t PROGMEM fn_actions[] = {};
